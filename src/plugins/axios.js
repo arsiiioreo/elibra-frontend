c137d642a93c7b544a170d15023797883599a7e2
Reign <reignromarchryzel.balico@isu.edu.ph>
2025-12-07 19:42:22 +0800

    updated route

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -3,19 +3,19 @@ import { token } from "@/stores/auth";
 import axios from "axios";
 
 const api = axios.create({
-    // baseURL: "https://antiquewhite-cassowary-936393.hostingersite.com/",
-    baseURL: "http://127.0.0.1:8000/",
+	baseURL: "https://antiquewhite-cassowary-936393.hostingersite.com/",
+	// baseURL: "http://127.0.0.1:8000/",
 });
 
 // Request interceptor: inject token if exists
 api.interceptors.request.use(
-    async (config) => {
-        if (token) {
-            config.headers.Authorization = `Bearer ${token.value}`;
-        }
-        return config;
-    },
-    (error) => Promise.reject(error)
+	async (config) => {
+		if (token) {
+			config.headers.Authorization = `Bearer ${token.value}`;
+		}
+		return config;
+	},
+	(error) => Promise.reject(error)
 );
 
 export default api;
